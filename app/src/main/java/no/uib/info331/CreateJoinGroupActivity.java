adddfd5855f8cb3c3b563aa57731e4084f740df8
eddiman <edbjorgen@gmail.com>
2017-09-29 00:26:40 +0200

    added join or create activity, and anims

--- app/src/main/java/no/uib/info331/CreateJoinGroupActivity.java
+++ app/src/main/java/no/uib/info331/CreateJoinGroupActivity.java
@@ -1,13 +1,125 @@
 package no.uib.info331;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.CardView;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.TextView;
 
+/**
+ * Micromanaging af here
+ *
+ * @author Edvard P. Bjørgen
+ *
+ */
 public class CreateJoinGroupActivity extends AppCompatActivity {
+
+    TextView title;
+
+    CardView chooseActionCard;
+    CardView joinGroupCard;
+    CardView createGroupCard;
+
+    Button btnJoinGroupShow;
+    Button btnCreateGroupShow;
+
+    boolean joinGroupBtnClicked;
+    boolean createGroupBtnClicked;
+
+    int longAnimTime;
+    Animation slideDownAnim;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_join_group);
+
+        initGui();
+        btnClickListener();
     }
+
+    private void initGui() {
+
+        title = (TextView) findViewById(R.id.text_create_join_group_title);
+
+        chooseActionCard = (CardView) findViewById(R.id.choose_action_card);
+        joinGroupCard = (CardView) findViewById(R.id.join_group_card);
+        createGroupCard = (CardView) findViewById(R.id.create_group_card);
+
+        btnJoinGroupShow = (Button) findViewById(R.id.show_join_group);
+        btnCreateGroupShow = (Button) findViewById(R.id.show_create_group);
+
+        longAnimTime = getResources().getInteger(android.R.integer.config_shortAnimTime);
+        slideDownAnim = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.down_slide);
+
+        joinGroupBtnClicked = false;
+        createGroupBtnClicked = false;
+
+    }
+
+    private void btnClickListener() {
+        btnJoinGroupShow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                joinGroupBtnClicked = true;
+                chooseActionCard.animate()
+                        .setDuration(longAnimTime)
+                        .alpha(joinGroupBtnClicked ? 0 : 1)
+                        .translationY(view.getHeight())
+                        .alpha(0.0f)
+                        .setListener(new AnimatorListenerAdapter() {
+                    @Override
+                    public void onAnimationEnd(Animator animation) {
+                        chooseActionCard.setVisibility(joinGroupBtnClicked ? View.GONE : View.VISIBLE);
+                    }
+                });
+
+
+            }
+        });
+
+
+        btnCreateGroupShow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                createGroupBtnClicked = true;
+                chooseActionCard.animate()
+                        .setDuration(longAnimTime)
+                        .alpha(joinGroupBtnClicked ? 0 : 1)
+                        .translationY(view.getHeight())
+                        .alpha(0.0f)
+                        .setListener(new AnimatorListenerAdapter() {
+                            @Override
+                            public void onAnimationEnd(Animator animation) {
+                                chooseActionCard.setVisibility(joinGroupBtnClicked ? View.GONE : View.VISIBLE);
+                            }
+                        });
+                joinGroupCard.animate()
+                        .setDuration(longAnimTime)
+                        .alpha(createGroupBtnClicked ? 0 : 1)
+                        .translationY(view.getHeight())
+                        .alpha(0.0f)
+                        .setListener(new AnimatorListenerAdapter() {
+                            @Override
+                            public void onAnimationEnd(Animator animation) {
+                                joinGroupCard.setVisibility(createGroupBtnClicked ? View.GONE : View.VISIBLE);
+                            }
+                        });
+
+
+            }
+        });
+
+
+    }
+
+
+
 }
