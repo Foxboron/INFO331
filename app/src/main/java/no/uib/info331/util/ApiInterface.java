f45747b58ba1283c5c5dea34bb1cce62735fc7f7
Fredrik Heimsæter <freheims@gmail.com>
2017-10-02 21:42:31 +0200

    Login and register speaks with backend

---
+++ app/src/main/java/no/uib/info331/util/ApiInterface.java
@@ -0,0 +1,25 @@
+package no.uib.info331.util;
+
+import okhttp3.ResponseBody;
+import retrofit2.Call;
+import retrofit2.Retrofit;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.POST;
+
+/**
+ * Created by fredrik on 10/1/17.
+ */
+
+public interface ApiInterface {
+
+    @FormUrlEncoded
+    @POST("/login")
+    Call<ResponseBody> login(@Field("username") String username, @Field("password") String password);
+
+    @FormUrlEncoded
+    @POST("/register")
+    Call<ResponseBody> register(@Field("username") String username, @Field("password") String password);
+
+
+}
