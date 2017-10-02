394084b6f583fd75a78b5ce64314478a54ee3418
eddiman <edbjorgen@gmail.com>
2017-10-03 00:37:53 +0200

    added chips collection to create group card

---
+++ app/src/main/java/no/uib/info331/util/MemberChip.java
@@ -0,0 +1,37 @@
+package no.uib.info331.util;
+
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
+
+import com.pchmn.materialchips.model.ChipInterface;
+
+/**
+ * Created by moled on 03.10.2017.
+ */
+
+public class MemberChip implements ChipInterface {
+    @Override
+    public Object getId() {
+        return null;
+    }
+
+    @Override
+    public Uri getAvatarUri() {
+        return null;
+    }
+
+    @Override
+    public Drawable getAvatarDrawable() {
+        return null;
+    }
+
+    @Override
+    public String getLabel() {
+        return null;
+    }
+
+    @Override
+    public String getInfo() {
+        return null;
+    }
+}
