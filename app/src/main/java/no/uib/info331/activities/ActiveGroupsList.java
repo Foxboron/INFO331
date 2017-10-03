cad2282c950590a485ef6d7a405142840404b796
Per-Niklas Longberg <perniklaslongberg@gmail.com>
2017-10-03 10:38:44 +0200

    Added xml for group items with layout to populate group list, added tiny test, still need to complete the custom adapter

--- app/src/main/java/no/uib/info331/activities/ActiveGroupsList.java
+++ app/src/main/java/no/uib/info331/activities/ActiveGroupsList.java
@@ -5,12 +5,14 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import no.uib.info331.R;
 import no.uib.info331.models.Group;
+import no.uib.info331.models.User;
 
 /**
  * Created by perni on 02.10.2017.
@@ -26,18 +28,26 @@ public class ActiveGroupsList extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_active_groups);
-        //groups = database.get(list_of_groups);
+        //groups = database.get(list_of_groups); haha hvordan skal dette gå gutter
 
+        ListView groupList = (ListView) findViewById(R.id.active_groups_list);
 
-    ListView groupList = (ListView) findViewById(R.id.active_groups_list);
+        //THE CODE BELOW IS USED FOR TESTING, DO NOT INCLUDE IN FINAL BUILD
+        User user1 = new User("user", "pass", "pic", 5);
+        User user2 = new User("user2", "pass11", "pic", 5);
+        User user3 = new User("user3", "pass1", "pic", 5);
 
+        Group group = new Group("testgroup", user1, 15);
 
-    //THE CODE BELOW IS USED FOR TESTING, DO NOT INCLUDE IN FINAL BUILD
-    //fikk ikke tid til å mekke as
-    //THE CODE ABOVE IS USED FOR TESTING, DO NOT INCLUDE IN FINAL BUILD
+        group.addMember(user2);
+        group.addMember(user3);
 
-    ArrayAdapter<Group> adapter = new ArrayAdapter<>(this.getApplicationContext(), R.layout.activity_active_groups, R.id.active_groups_list, groups);
-    groupList.setAdapter(adapter);
+        TextView test = (TextView) findViewById(R.id.groupName);
+        test.setText(group.getName());
+        //THE CODE ABOVE IS USED FOR TESTING, DO NOT INCLUDE IN FINAL BUILD
+
+        ArrayAdapter<Group> adapter = new ArrayAdapter<>(this.getApplicationContext(), R.layout.activity_active_groups, R.id.active_groups_list, groups);
+        groupList.setAdapter(adapter);
 
     }
 }
