eaf9af51f6cb3a4284e072a9de675d926995286f
eddiman <edbjorgen@gmail.com>
2017-10-03 20:55:02 +0200

    changes in create group

--- app/src/main/java/no/uib/info331/activities/CreateJoinGroupActivity.java
+++ app/src/main/java/no/uib/info331/activities/CreateJoinGroupActivity.java
@@ -2,24 +2,28 @@ package no.uib.info331.activities;
 
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
-import android.animation.ObjectAnimator;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.CardView;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.pchmn.materialchips.ChipsInput;
 import com.pchmn.materialchips.model.Chip;
+import com.pchmn.materialchips.model.ChipInterface;
 
 import java.util.ArrayList;
-import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import no.uib.info331.R;
+import no.uib.info331.util.Animations;
 
 /**
  * Micromanaging af here
@@ -31,26 +35,32 @@ public class CreateJoinGroupActivity extends AppCompatActivity {
 
     @BindView(R.id.text_create_join_group_title) TextView textViewTitle;
 
+    private float topYOfParentLayout;
+
+    //ButterKnife gui
+
     @BindView(R.id.choose_action_card) CardView chooseActionCard;
-
-
     @BindView(R.id.join_group_card) CardView joinGroupCard;
+
     @BindView(R.id.create_group_card) CardView createGroupCard;
-
     @BindView(R.id.show_join_group) Button btnJoinGroupShow;
-    @BindView(R.id.show_create_group) Button btnCreateGroupShow;
 
+    @BindView(R.id.show_create_group) Button btnCreateGroupShow;
     @BindView(R.id.join_search_group) EditText editTextSearchJoinGroup;
+
     @BindView(R.id.chips_input) ChipsInput chipsInput;
+    @BindView(R.id.add_group_name) LinearLayout addGroupNameLayout;
+
 
     boolean joinGroupBtnClicked;
-
     boolean createGroupBtnClicked;
-    int shortAnimTime;
 
+    int shortAnimTime;
     int mediumAnimTime;
 
-    //Butterknfe gui
+    Animations anim = new Animations();
+    private ArrayList<Chip> contactList;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -59,18 +69,71 @@ public class CreateJoinGroupActivity extends AppCompatActivity {
         ButterKnife.bind(this);
         initGui();
         btnClickListener();
+
     }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        // TODO Auto-generated method stub
+        super.onWindowFocusChanged(hasFocus);
+        if (hasFocus) {
+            topYOfParentLayout = 0 - (chipsInput.getY() + createGroupCard.getY());
+
+            chipsInput.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    System.out.println("TESTETETE");
+
+                }
+            });
+
+            chipsInput.addChipsListener(new ChipsInput.ChipsListener() {
+                @Override
+                public void onChipAdded(ChipInterface chip, int newSize) {
+                    // chip added
+                    // newSize is the size of the updated selected chip list
+                }
+
+                @Override
+                public void onChipRemoved(ChipInterface chip, int newSize) {
+                    // chip removed
+                    // newSize is the size of the updated selected chip list
+                }
+
+                @Override
+                public void onTextChanged(CharSequence text) {
+                    // text changed
+                    anim.moveViewToTranslationY(chipsInput,0, 200, Math.round(topYOfParentLayout), false);
+                    anim.fadeBackgroundFromColorToColor(chipsInput, 200, ContextCompat.getColor(getApplicationContext(), R.color.colorTransp), Color.WHITE);
+                    chipsInput.setBackgroundColor(Color.WHITE);
+                    if(text.length() == 0){
+                        anim.moveViewToTranslationY(chipsInput,0, 200, Math.round(0), false);
+                        anim.fadeBackgroundFromColorToColor(chipsInput, 200, Color.WHITE, ContextCompat.getColor(getApplicationContext(), R.color.colorTransp));
+                        chipsInput.setBackgroundColor(ContextCompat.getColor(getApplicationContext(), R.color.colorTransp));
+
+                    }
+                }
+            });
+
+
+        }
+    }
+
 
     private void initGui() {
         shortAnimTime = getResources().getInteger(android.R.integer.config_shortAnimTime);
         mediumAnimTime = getResources().getInteger(android.R.integer.config_mediumAnimTime);
 
+        System.out.println("initgui"+ chipsInput.getY());
+
+
+
         joinGroupBtnClicked = false;
         createGroupBtnClicked = false;
 
-        fadeInView(textViewTitle, 200, shortAnimTime);
+        anim.fadeInView(textViewTitle, 200, shortAnimTime);
 
-        List<Chip> contactList = new ArrayList<>();
+        contactList = new ArrayList<>();
         contactList.add(new Chip("Mega", "Magnus"));
         contactList.add(new Chip("Bjarne", "Betjent"));
         contactList.add(new Chip("Taco", "Tveit"));
@@ -78,40 +141,25 @@ public class CreateJoinGroupActivity extends AppCompatActivity {
 
         chipsInput.setFilterableList(contactList);
 
+        getWindow().setSoftInputMode(
+                WindowManager.LayoutParams.SOFT_INPUT_ADJUST_PAN);
+
+
     }
 
-    private void fadeInView(View view, int startDelay, int duration) {
-        view.setAlpha(0);
-        ObjectAnimator fadeIn = ObjectAnimator.ofFloat(view, "alpha",  0, 1f);
-        fadeIn.setStartDelay(startDelay);
-        fadeIn.setDuration(duration);
-        fadeIn.start();
-    }
-    private void fadeOutView(View view, int startDelay, int duration) {
-        ObjectAnimator fadeIn = ObjectAnimator.ofFloat(view, "alpha",  1f, 0);
-        fadeIn.setStartDelay(startDelay);
-        fadeIn.setDuration(duration);
-        fadeIn.start();
-    }
+
 
     private void btnClickListener() {
+
+
         btnJoinGroupShow.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
                 joinGroupBtnClicked = true;
-                chooseActionCard.animate()
-                        .setDuration(shortAnimTime)
-                        .alpha(joinGroupBtnClicked ? 0 : 1)
-                        .translationY(view.getHeight())
-                        .alpha(0.0f)
-                        .setListener(new AnimatorListenerAdapter() {
-                            @Override
-                            public void onAnimationEnd(Animator animation) {
-                                chooseActionCard.setVisibility(joinGroupBtnClicked ? View.GONE : View.VISIBLE);
-                            }
-                        });
 
+                anim.fadeOutView(chooseActionCard, 0, shortAnimTime);
+                anim.moveViewToTranslationY(chooseActionCard,0 , shortAnimTime, chooseActionCard.getHeight(), true);
 
             }
         });
@@ -122,32 +170,12 @@ public class CreateJoinGroupActivity extends AppCompatActivity {
             public void onClick(View view) {
 
                 createGroupBtnClicked = true;
-                chooseActionCard.animate()
-                        .setDuration(shortAnimTime)
-                        .translationY(view.getHeight())
-                        .alpha(0.0f)
-                        .setListener(new AnimatorListenerAdapter() {
-                            @Override
-                            public void onAnimationEnd(Animator animation) {
-                                chooseActionCard.setVisibility(View.GONE);
-                                chooseActionCard.animate()
-                                        .translationY(chooseActionCard.getHeight());
-                            }
-                        });
-                joinGroupCard.animate()
-                        .setDuration(shortAnimTime)
-                        .translationY(view.getHeight())
-                        .alpha(0.0f)
-                        .setStartDelay(200)
-                        .setListener(new AnimatorListenerAdapter() {
-                            @Override
-                            public void onAnimationEnd(Animator animation) {
-                                joinGroupCard.setVisibility(View.GONE);
-                                joinGroupCard.animate()
-                                        .translationY(joinGroupCard.getHeight());
-                            }
-                        });
 
+                anim.moveViewToTranslationY(chooseActionCard, 100 , mediumAnimTime, chooseActionCard.getHeight(), true);
+                anim.fadeOutView(chooseActionCard, 0, shortAnimTime);
+
+                anim.moveViewToTranslationY(joinGroupCard, 200, mediumAnimTime, joinGroupCard.getHeight(), true);
+                anim.fadeOutView(joinGroupCard, 0, shortAnimTime);
 
             }
         });
@@ -160,49 +188,21 @@ public class CreateJoinGroupActivity extends AppCompatActivity {
 
         if(joinGroupBtnClicked) {
             joinGroupBtnClicked = false;
-            //Animates the card for choosing what to do
 
-            chooseActionCard.animate()
-                    .alpha(1.0f)
-                    .setListener(new AnimatorListenerAdapter() {
-                        @Override
-                        public void onAnimationEnd(Animator animation) {
-                            chooseActionCard.setVisibility(View.VISIBLE);
-                            chooseActionCard.animate()
-                                    .setDuration(shortAnimTime)
-                                    .translationY(0);
-                        }
-                    });
+            //Animates the card for choosing what to do
+            anim.fadeInView(chooseActionCard, 0, shortAnimTime);
+            anim.moveViewToTranslationY(chooseActionCard, 50 , shortAnimTime, 0, false);
+
         } else if(createGroupBtnClicked) {
             joinGroupBtnClicked = false;
             createGroupBtnClicked = false;
+
             //Animates the card for choosing what to do
-            chooseActionCard.animate()
-                    .setDuration(shortAnimTime)
-                    .alpha(1.0f)
-                    .setListener(new AnimatorListenerAdapter() {
-                        @Override
-                        public void onAnimationEnd(Animator animation) {
-                            chooseActionCard.setVisibility(View.VISIBLE);
-                            chooseActionCard.animate()
-                                    .translationY(0)
-                                    .alpha(1.0f);
-                        }
-                    });
+            anim.fadeInView(joinGroupCard, 0, shortAnimTime);
+            anim.moveViewToTranslationY(joinGroupCard, 100 , shortAnimTime, 0, false);
 
-            joinGroupCard.animate()
-                    .setDuration(shortAnimTime)
-
-                    .setStartDelay(100)
-                    .setListener(new AnimatorListenerAdapter() {
-                        @Override
-                        public void onAnimationEnd(Animator animation) {
-                            joinGroupCard.setVisibility(View.VISIBLE);
-                            joinGroupCard.animate()
-                                    .translationY(0)
-                                    .alpha(1.0f);
-                        }
-                    });
+            anim.fadeInView(chooseActionCard, 0, shortAnimTime);
+            anim.moveViewToTranslationY(chooseActionCard, 200 , shortAnimTime, 0, false);
         }  else{
 
             super.onBackPressed();
---
+++ app/src/main/java/no/uib/info331/util/Animations.java
@@ -0,0 +1,76 @@
+package no.uib.info331.util;
+
+import android.animation.Animator;
+import android.animation.ObjectAnimator;
+import android.content.Context;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.TransitionDrawable;
+import android.support.design.widget.BaseTransientBottomBar;
+import android.support.v4.content.ContextCompat;
+import android.view.View;
+
+import no.uib.info331.R;
+
+/**
+ * Created by moled on 03.10.2017.
+ */
+
+public class Animations {
+
+    public void fadeInView(View view, int startDelay, int duration) {
+        view.setAlpha(0);
+        ObjectAnimator fadeIn = ObjectAnimator.ofFloat(view, "alpha",  0, 1f);
+        fadeIn.setStartDelay(startDelay);
+        fadeIn.setDuration(duration);
+        fadeIn.start();
+    }
+    public void fadeOutView(View view, int startDelay, int duration) {
+        ObjectAnimator fadeIn = ObjectAnimator.ofFloat(view, "alpha",  1f, 0);
+        fadeIn.setStartDelay(startDelay);
+        fadeIn.setDuration(duration);
+        fadeIn.start();
+    }
+
+    public void moveViewToTranslationY(final View VIEW, int startDelay, int duration, int offset, final boolean goneOnAnimEnd) {
+        ObjectAnimator fadeIn = ObjectAnimator.ofFloat(VIEW, "TranslationY", offset);
+        fadeIn.setStartDelay(startDelay);
+        fadeIn.setDuration(duration);
+        fadeIn.start();
+        fadeIn.addListener(new Animator.AnimatorListener() {
+            @Override
+            public void onAnimationStart(Animator animation) {
+                if(!goneOnAnimEnd){
+                    VIEW.setVisibility(View.VISIBLE);
+                }
+            }
+
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                if(goneOnAnimEnd){
+                    VIEW.setVisibility(View.GONE);
+                }
+            }
+
+            @Override
+            public void onAnimationCancel(Animator animation) {
+
+            }
+
+            @Override
+            public void onAnimationRepeat(Animator animation) {
+
+            }
+        });
+    }
+
+    public void fadeBackgroundFromColorToColor(View view, int duration, int fromColor, int toColor){
+        ColorDrawable[] color2 = {new ColorDrawable(fromColor), new ColorDrawable(toColor)};
+        TransitionDrawable trans = new TransitionDrawable(color2);
+        //This will work also on old devices. The latest API says you have to use setBackground instead.
+        view.setBackground(trans);
+        trans.startTransition(duration);
+
+    }
+
+}
