148c7932ee0ee1a7b61844d96189dc346168fd93
EddiStat <moledda@gmail.com>
2017-10-04 22:57:13 +0200

    added get all users from db

--- app/src/main/java/no/uib/info331/activities/LoginActivity.java
+++ app/src/main/java/no/uib/info331/activities/LoginActivity.java
@@ -5,17 +5,10 @@ import android.animation.AnimatorListenerAdapter;
 import android.annotation.TargetApi;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
-import android.app.LoaderManager.LoaderCallbacks;
 
-import android.content.CursorLoader;
-import android.content.Loader;
-import android.database.Cursor;
-import android.net.Uri;
-import android.os.AsyncTask;
 
 import android.os.Build;
 import android.os.Bundle;
-import android.provider.ContactsContract;
 import android.text.TextUtils;
 import android.view.KeyEvent;
 import android.view.View;
