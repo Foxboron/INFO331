aa7edfb22cb9794ea8c2f1e58d48608ce45972a7
Per-Niklas Longberg <perniklaslongberg@gmail.com>
2017-10-04 13:39:14 +0200

    Code cleaning

--- app/src/main/java/no/uib/info331/models/GroupAdapter.java
+++ app/src/main/java/no/uib/info331/models/GroupAdapter.java
@@ -12,27 +12,19 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
-import java.util.List;
 
 import no.uib.info331.R;
 
-
-
 /**
- * Created by perni on 02.10.2017.
+ * Created by Per-Niklas Longberg on 02.10.2017.
  *
- * This class should work because everyone on the internet says that just a normal adapter won't
- * work with objects in ListView
+ * Custom adapter for populating ListView with our group objects.
  *
- * Still a work in progress - reading up on guides and figuring out what's what at the moment
- * 
  **/
-
 
 public class GroupAdapter extends ArrayAdapter<Group> {
 
     private Context context;
-    private int layoutResourceID;
     private ArrayList<Group> groupList;
     private int lastPosition = -1;
 
@@ -42,82 +34,26 @@ public class GroupAdapter extends ArrayAdapter<Group> {
         this.groupList = groups;
     }
 
-    /**
-     * Original constructor - may be obsolete but tests need to be run on new
-     *
-    public GroupAdapter(Context context, int resource, List<Group> groups){
-        super(context, resource, groups);
-        this.context = context;
-        this.layoutResourceID = resource;
-        this.groupList = (ArrayList<Group>) groups;
-    }
-
-     **/
-
     private static class ViewHolder {
         TextView grpName;
         ImageView info;
     }
 
-
-    /**
-     * Original getView method - may be obsolete but will not delete until tests have been run on new method
-     *
-    @NonNull
-    @Override
-    public View getView(int position, View convertView, @NonNull ViewGroup parent) {
-        View row = convertView;
-        RecyclerView.ViewHolder holder = null;
-        Group data = groupList.get(position);
-
-        if(row == null)
-        {
-            LayoutInflater inflater = ((Activity)context).getLayoutInflater();
-            row = inflater.inflate(layoutResourceID, parent, false);
-
-
-
-            row.setTag(holder);
-        }
-        else
-        {
-            holder = (HeaderHolder) row.getTag();
-        }
-
-        Group group = data.get(position);
-        holder.from.setText(item.getFrom());
-        holder.to.setText(item.getTo());
-        holder.subject.setText(item.getSubject());
-
-        return row;
-    }
-
-    private class HeaderHolder {
-        public TextView from;
-        public TextView to;
-        public TextView subject;
-    }
-    **/
-
     @NonNull
     public View getView(int position, View convertView, @NonNull ViewGroup parent) {
-        // Get the data item for this position
         Group data = groupList.get(position);
-        // Check if an existing view is being reused, otherwise inflate the view
-        ViewHolder viewHolder; // view lookup cache stored in tag
-
+        ViewHolder viewHolder;
         final View result;
 
         if (convertView == null) {
-
             viewHolder = new ViewHolder();
             LayoutInflater inflater = LayoutInflater.from(getContext());
             convertView = inflater.inflate(R.layout.group_list_element, parent, false);
+
             viewHolder.grpName = (TextView) convertView.findViewById(R.id.groupName);
             viewHolder.info = (ImageView) convertView.findViewById(R.id.groupIcon);
 
-            result=convertView;
-
+            result = convertView;
             convertView.setTag(viewHolder);
         } else {
             viewHolder = (ViewHolder) convertView.getTag();
@@ -130,7 +66,7 @@ public class GroupAdapter extends ArrayAdapter<Group> {
 
         viewHolder.grpName.setText(data.getName());
         viewHolder.info.setTag(position);
-        // Return the completed view to render on screen
+
         return convertView;
     }
 }
