df4b89a7f386f05689bf4fe9f5a311342a7c3102
perfjes <perniklaslongberg@gmail.com>
2017-10-05 12:18:56 +0200

    Added group profile page with list of members (expandableListView fucks up)

--- app/src/main/java/no/uib/info331/activities/GroupProfilePage.java
+++ app/src/main/java/no/uib/info331/activities/GroupProfilePage.java
@@ -3,13 +3,16 @@ package no.uib.info331.activities;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ArrayAdapter;
+import android.widget.ExpandableListView;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
 import no.uib.info331.R;
 import no.uib.info331.models.Group;
 import no.uib.info331.models.User;
+import no.uib.info331.models.UserAdapter;
 
 /**
  * Created by perni on 04.10.2017.
@@ -18,12 +21,31 @@ import no.uib.info331.models.User;
 public class GroupProfilePage extends AppCompatActivity {
     private ArrayList<User> users;
     private Group currentGroup;
+    private ListView userList;
+    private TextView points;
+    private TextView gname;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_group_profile);
+        users = new ArrayList<>();
+        User u = new User("haha", "yeye", "haha", 5);
+        for(int i = 0; i < 10; i++){
+            users.add(u);
+        }
+        users.add(u);
 
+        currentGroup = new Group("Groupname haha", u, 5000);
+        points = (TextView) findViewById(R.id.group_points);
+        points.setText(Integer.toString(currentGroup.getPoints()));
+        gname = (TextView) findViewById(R.id.group_display_name);
+        gname.setText(currentGroup.getName());
+
+        userList = (ExpandableListView) findViewById(R.id.exp_mem_list_grp);
+
+        UserAdapter adapt = new UserAdapter(users, this.getApplicationContext());
+        userList.setAdapter(adapt);
 
     }
 }
---
+++ app/src/main/java/no/uib/info331/models/UserAdapter.java
@@ -0,0 +1,73 @@
+package no.uib.info331.models;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+import no.uib.info331.R;
+
+/**
+ * Created by Per-Niklas Longberg on 02.10.2017.
+ *
+ * Custom adapter for populating ListView with our group objects.
+ *
+ **/
+
+public class UserAdapter extends ArrayAdapter<User> {
+
+    private Context context;
+    private ArrayList<User> userList;
+    private int lastPosition = -1;
+
+    public UserAdapter(ArrayList<User> users, Context context){
+        super(context, R.layout.group_member_list_child, users);
+        this.context = context;
+        this.userList = users;
+    }
+
+    private static class ViewHolder {
+        TextView grpName;
+        ImageView info;
+    }
+
+    @NonNull
+    public View getView(int position, View convertView, @NonNull ViewGroup parent) {
+        User data = userList.get(position);
+        ViewHolder viewHolder;
+        final View result;
+
+        if (convertView == null) {
+            viewHolder = new ViewHolder();
+            LayoutInflater inflater = LayoutInflater.from(getContext());
+            convertView = inflater.inflate(R.layout.group_member_list_child, parent, false);
+
+            // Må konverteres til å finne usernavn
+            viewHolder.grpName = (TextView) convertView.findViewById(R.id.group_member_list_username);
+            viewHolder.info = (ImageView) convertView.findViewById(R.id.user_profile_pic);
+
+            result = convertView;
+            convertView.setTag(viewHolder);
+        } else {
+            viewHolder = (ViewHolder) convertView.getTag();
+            result=convertView;
+        }
+
+        //Animation animation = AnimationUtils.loadAnimation(context, (position > lastPosition) ? R.anim.up_from_bottom : R.anim.down_from_top);
+        //result.startAnimation(animation);
+        //lastPosition = position;
+
+        viewHolder.grpName.setText(data.getUsername());
+        viewHolder.info.setTag(position);
+
+        return convertView;
+    }
+}
