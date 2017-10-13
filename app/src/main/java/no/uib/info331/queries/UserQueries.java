86d2f3bb8cb92d3670683cc11e6cd8c2de47c3fa
Fredrik Heimsæter <freheims@gmail.com>
2017-10-13 19:03:17 +0200

    Added docs for UserQueries and ApiClient

--- app/src/main/java/no/uib/info331/queries/UserQueries.java
+++ app/src/main/java/no/uib/info331/queries/UserQueries.java
@@ -18,7 +18,8 @@ import retrofit2.Callback;
 import retrofit2.Response;
 
 /**
- * Created by moled on 05.10.2017.
+ * User-related queries against the backend
+ * @author Edvard P. Bjørgen
  */
 
 public class UserQueries {
@@ -26,6 +27,12 @@ public class UserQueries {
     ApiInterface apiService = ApiClient.getClient().create(ApiInterface.class);
     DataManager dataManager = new DataManager();
 
+    /**
+     * Search for users in the db and save the results in sharedPreferences
+     * @param context The activity context
+     * @param query The search query
+     * @return The list of users where the username matches the query
+     */
     public List<User> getUsersByStringFromDb(final Context context, String query) {
         //username:password
         User signedInUser = dataManager.getSavedObjectFromSharedPref(context, "currentlySignedInUser", new TypeToken<User>(){}.getType());
@@ -70,6 +77,10 @@ public class UserQueries {
         return dataManager.getSavedObjectFromSharedPref(context, prefKey, type);
     }
 
+    /**
+     * Get all the users from the database and print them to the console
+     * @param CONTEXT The activity context
+     */
     private void getAllUsersFromDb(final Context CONTEXT) {
 
         ApiInterface apiService = ApiClient.getClient().create(ApiInterface.class);
