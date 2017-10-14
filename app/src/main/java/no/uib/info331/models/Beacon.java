849748a7b070ca07e89bd27a207b2bd1df507285
Fredrik Heimsæter <freheims@gmail.com>
2017-10-14 16:07:12 +0200

    Updated beacon model

--- app/src/main/java/no/uib/info331/models/Beacon.java
+++ app/src/main/java/no/uib/info331/models/Beacon.java
@@ -16,8 +16,11 @@ public class Beacon {
     private int latitude;
     private int longitude;
 
-    public Beacon(String ID, String name, int latitude, int longitude) {
+    public Beacon(String ID, String UUID, String major, String minor, String name, int latitude, int longitude) {
         this.ID = ID;
+        this.UUID = UUID;
+        this.major = major;
+        this.minor = minor;
         this.name = name;
         this.latitude = latitude;
         this.longitude = longitude;
@@ -25,8 +28,35 @@ public class Beacon {
     }
 
     public String getID() {
-        //TODO Create UUID, major and minor from ID
         return ID;
+    }
+
+    public void setID(String ID) {
+        this.ID = ID;
+    }
+
+    public String getUUID() {
+        return UUID;
+    }
+
+    public void setUUID(String UUID) {
+        this.UUID = UUID;
+    }
+
+    public String getMajor() {
+        return major;
+    }
+
+    public void setMajor(String major) {
+        this.major = major;
+    }
+
+    public String getMinor() {
+        return minor;
+    }
+
+    public void setMinor(String minor) {
+        this.minor = minor;
     }
 
     public String getName() {
@@ -51,5 +81,41 @@ public class Beacon {
 
     public void setLongitude(int longitude) {
         this.longitude = longitude;
+    }
+
+    @Override
+    public String toString() {
+        return "Beacon{" +
+                "ID='" + ID + '\'' +
+                ", UUID='" + UUID + '\'' +
+                ", major='" + major + '\'' +
+                ", minor='" + minor + '\'' +
+                ", name='" + name + '\'' +
+                ", latitude=" + latitude +
+                ", longitude=" + longitude +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Beacon beacon = (Beacon) o;
+
+        if (!ID.equals(beacon.ID)) return false;
+        if (!UUID.equals(beacon.UUID)) return false;
+        if (!major.equals(beacon.major)) return false;
+        return minor.equals(beacon.minor);
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = ID.hashCode();
+        result = 31 * result + UUID.hashCode();
+        result = 31 * result + major.hashCode();
+        result = 31 * result + minor.hashCode();
+        return result;
     }
 }
