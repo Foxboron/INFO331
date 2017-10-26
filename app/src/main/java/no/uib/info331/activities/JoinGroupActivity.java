6260262136b5e42eafa26f716354644454bed628
eddiman <edbjorgen@gmail.com>
2017-10-26 18:29:59 +0200

    started adding components into JoinGroupActivity.java

--- app/src/main/java/no/uib/info331/activities/JoinGroupActivity.java
+++ app/src/main/java/no/uib/info331/activities/JoinGroupActivity.java
@@ -2,10 +2,26 @@ package no.uib.info331.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.CardView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
+import butterknife.BindView;
 import no.uib.info331.R;
 
 public class JoinGroupActivity extends AppCompatActivity {
+
+    @BindView(R.id.join_group_card) CardView cardJoinGroup;
+
+
+    @BindView(R.id.edittext_search_for_groups) EditText editTextSearchForGroups;
+    @BindView(R.id.listview_add_group_list) ListView listViewGroupList;
+    @BindView(R.id.imagebutton_search_for_group) ImageButton imageBtnSearchForGroups;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
